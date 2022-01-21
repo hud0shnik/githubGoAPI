fff7af8e7c04404de408d863596ce1ae08ced8a4
Danila Egorov <c4nsero@mail.ru>
2022-01-21 12:42:46 +0300

    update comments
    Немного обновил комментарии

--- main.go
+++ main.go
@@ -20,21 +20,27 @@ type User struct {
 }
 
 func getCommits(username string) User {
+	// Вывод в терминал для тестов
 	fmt.Println("Checking user: " + username)
+
 	// Формирование и исполнение запроса
 	resp, err := http.Get("https://github.com/" + username)
 	if err != nil {
 		fmt.Println("Github error: ", err)
 		return User{}
 	}
+
 	// Запись информации из респонса
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
 
+	// Получение сегодняшней даты
 	currentDate := string(time.Now().Format("2006-01-02"))
 
 	// Вот так выглядит html одной ячейки:
 	// <rect width="11" height="11" x="-36" y="75" class="ContributionCalendar-day" rx="2" ry="2" data-count="1" data-date="2021-12-03" data-level="1"></rect>
+
+	// Поиск сегодняшней ячейеки
 	if strings.Contains(string(body), "data-date=\""+currentDate+"\" data-level=\"") {
 		pageStr, commitsString := string(body), ""
 		i := 0
@@ -51,12 +57,12 @@ func getCommits(username string) User {
 			// Доводит i до символа "
 		}
 		for i++; pageStr[i] != '"'; i++ {
-			// Считывание значения в скобках
+			// Считывание и запись значения в скобках
 			commitsString += string(pageStr[i])
 		}
 		for i += 35; pageStr[i] != '"'; i++ {
 		}
-		// Запись и обработка полученной информации (цвет клетки)
+		// Запись и обработка полученной информации
 		dataLevel, _ := strconv.Atoi(pageStr[i+1 : i+2])
 		commits, _ := strconv.Atoi(commitsString)
 		return User{
@@ -73,18 +79,25 @@ func getCommits(username string) User {
 }
 
 func getUser(writer http.ResponseWriter, request *http.Request) {
+	// Заголовок, определяющий тип данных для работы
 	writer.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(request)
-	result := getCommits(params["id"])
 
+	// Запись параметров из реквеста
+	params := mux.Vars(request)
+
+	// Обработка данных и вывод результата
+	result := getCommits(params["id"])
 	json.NewEncoder(writer).Encode(result)
 }
 
 func main() {
+	// Вывод даты начала работы
 	fmt.Println("API Start:" + string(time.Now().Format("2006-01-02 15:04:05")))
+
+	// Роутер
 	router := mux.NewRouter()
 
+	// Маршрут user
 	router.HandleFunc("/user/{id}", getUser).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8000", router))
-
 }
